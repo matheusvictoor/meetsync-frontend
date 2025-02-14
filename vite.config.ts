58967dc759dc66611e05f2e3b9046e793491ade1
Matheus Victor <matheusvictor.dev@gmail.com>
2025-02-14 17:03:10 -0300

    chore: instala e configura as dependencias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react().filter(Boolean),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
