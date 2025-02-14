53cb91f8a7b335a5686fdb865262429b5db5f11e
Matheus Victor <matheusvictor.dev@gmail.com>
2025-02-14 18:38:56 -0300

    feat: adiciona components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Home";
 import CreateRoom from "./pages/CreateRoom";
-// import VotingRoom from "./pages/VotingRoom";
+import VotingRoom from "./pages/VotingRoom";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -15,7 +15,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/criar-sala" element={<CreateRoom />} />
-          {/* <Route path="/sala-votacao/:roomId" element={<VotingRoom />} /> */}
+          <Route path="/sala-votacao/:roomId" element={<VotingRoom />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
