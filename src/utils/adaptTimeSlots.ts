0a4d84f08a38407ab5edba77ed1d0181d17a661e
Matheus Victor <matheusvictor.dev@gmail.com>
2025-02-14 18:44:51 -0300

    chore: funcao para conversao de datas

--- src/utils/adaptTimeSlots.ts
+++ src/utils/adaptTimeSlots.ts
@@ -1,40 +1,7 @@
 import { toZonedTime } from 'date-fns-tz';
 import { parseISO } from 'date-fns';
-
-interface Vote {
-  name: string;
-  email?: string;
-  timeSlots: string[];
-}
-
-interface TimeSlot {
-  dateTime: string;
-  duration: string;
-  votes: Vote[];
-}
-
-type RoomAPIResponse = {
-  roomId: string;
-  title?: string;
-  description?: string;
-  emails: string[];
-  endingAt: string;
-  createdAt: string;
-  updatedAt: string;
-  Time: {
-    roomId: string;
-    timeId: string;
-    date: string;
-    start: string;
-    end: string;
-    Vote: {
-      voteId: string;
-      createdAt: string;
-      userName: string;
-      timeId: string;
-    }[];
-  }[];
-};
+import { RoomAPIResponse } from '@/types/room';
+import { TimeSlot } from '@/types/vote';
 
 export default function adaptTimeSlots(apiRoom: RoomAPIResponse): TimeSlot[] {
   return apiRoom.Time.map((time) => {
