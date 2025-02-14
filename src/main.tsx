5f41975f7a478f9a82e4b3bb69c6a0b673edee42
Matheus Victor <matheusvictor.dev@gmail.com>
2025-02-14 17:04:49 -0300

    feat: adicona o arquivo main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App.tsx';
+import './index.css';
+import { Toaster } from 'sonner';
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <StrictMode>
+    <Toaster position="top-center" richColors  />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
